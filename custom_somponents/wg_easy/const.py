94d801538bc32d7cb39b3e309422c455a8c71ebb
metaathron <metaathron@server>
2026-03-19 15:00:25 +0100

    Fix HACS repository structure,

--- custom_somponents/wg_easy/const.py
+++
@@ -1,6 +0,0 @@
-DOMAIN = "wg_easy"
-DEFAULT_POLL_INTERVAL = 30
-DEFAULT_ONLINE_TIMEOUT_MINUTES = 5
-PLATFORMS = ["sensor", "binary_sensor"]
-SERVER_DEVICE_ID = "wireguard_server"
-ENTITY_ID_PREFIX = "wg"
